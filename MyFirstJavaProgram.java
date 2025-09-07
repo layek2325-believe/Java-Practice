db8c38b45329fe8ec8f496ac5484766c55eb51a5
Goutam Layek <layek2325@gmail.com>
2025-09-07 15:27:15 +0530

    Added My Firt Java code

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,33 @@
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("\n Hello Goutam All the best , just be consistent"); // prints Hello World
+   }
+}
+
+/*Class Names − For all class names the first letter should be in Upper Case. If several words are used to form a name of the class, each inner word's first letter should be in Upper Case.
+
+Example − class MyFirstJavaClass
+
+Method Names − All method names should start with a Lower Case letter. If several words are used to form the name of the method, then each inner word's first letter should be in Upper Case.
+
+Example − public void myMethodName()
+
+Program File Name − Name of the program file should exactly match the class name.
+
+When saving the file, you should save it using the class name (Remember Java is case sensitive) and append '.java' to the end of the name (if the file name and the class name do not match, your program will not compile).
+
+But please make a note that in case you do not have a public class present in the file then file name can be different than class name. It is also not mandatory to have a public class in the file.
+
+Example − Assume 'MyFirstJavaProgram' is the class name. Then the file should be saved as 'MyFirstJavaProgram.java'
+
+public static void main(String args[]) − Java program processing starts from the main() method which is a mandatory part of every Java program. */
+
+
+
+// to compile   javac MyFirstJavaProgram.java
+// to run       java MyFirstJavaProgram
