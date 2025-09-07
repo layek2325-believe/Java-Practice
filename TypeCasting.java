3cad8d54f6da3f350cac993f75d3b68b22e49029
Goutam Layek <layek2325@gmail.com>
2025-09-07 23:21:51 +0530

    Added Third Program TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,20 @@
+public class TypeCasting{
+
+    // in this program we will convert double type to integer type
+
+    public static void main(String args[]){
+
+        double num1 = 500;
+        int num2 = 10, sum;
+
+        int intNum1 = (int)num1;
+
+        sum = intNum1 + num2;
+
+        System.out.println("num1 before TypeCasting : " + num1 + " After : " + intNum1);
+        System.out.println("sum : " + sum);
+
+    }
+
+}
+
